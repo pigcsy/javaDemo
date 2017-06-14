588332ed5e59d3bfebbf305fe50cc17d597ef14c
csy <458721928@qq.com>
2017-06-14 18:55:35 +0800

    工厂模式和单例模式 demo

--- web/src/main/java/com/third/Test.java
+++ web/src/main/java/com/third/Test.java
@@ -8,4 +8,25 @@ public class Test {
         public void doSomething();
     }
 
-}
+    public static void main(String[] args) {
+            new Thread(new Runnable() {
+                public volatile int count = 0;
+
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100; i++) {
+                        //这里延迟1毫秒，使得结果明显
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                        }
+                        count++;
+                        System.out.println("--------" + count);
+                    }
+                }
+            }).start();
+
+        }
+
+    }
+
